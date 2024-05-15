7a736eadde3fe7c0e918ac6a0fd324ca8adf12c2
Turabi47 <aktura47@gmail.com>
2024-05-15 18:34:13 +0500

    sdkf

--- first.js
+++ first.js
@@ -1,6 +1,44 @@
 let a = 10
 console.log(a)
 
-let b = null
+let b = 34
 console.log(typeof(b))
 console.log(b)
+
+const student = {
+    Name : "Abdul Aziz",
+    age : 24,
+    email : "akturabi@gmail.com"
+
+}
+
+student.Name = "Abdul Aziz Khan"
+console.log(student.Name)
+
+const Name = "Zahid"
+// Name = "Zahid Saeed"
+console.log(Name)
+
+var stud = {
+    Name : "Zahid",
+    rNo : 123,
+    age : 23
+
+}
+stud.age = 22
+console.log(stud)
+
+let c = "sdf"
+c = 32
+
+console.log("Type of c is:",typeof(c))
+
+function myfunction() {
+    console.log(c)
+}
+myfunction()
+
+
+z = 20
+
+console.log(z)
