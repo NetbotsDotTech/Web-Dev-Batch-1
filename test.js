1eaa5e40cd4311ba3358d7cd7cd4b56a253ea00d
Ehsan Ali <ehsanpkd.19@gmail.com>
2024-05-15 06:31:05 -0700

    add java file

---
+++ test.js
@@ -0,0 +1,41 @@
+
+
+// var num1 =10;
+// var num1 =30;
+// console.log(num1)
+
+// const num2 =10;
+     
+// console.log(num2)
+
+// let num3 =5;
+//     num3 =30;
+// console.log(num3)
+
+
+var Trainee={
+    age:20,
+    name:"Ali",
+    email:"ali@gmail.com"
+}
+
+Trainee.name=40;
+console.log(Trainee)
+
+const Students={
+    age:10,
+    name:"Ahmad",
+    email:"ali@gmail.com"
+}
+
+Students.name="Ehsan";
+console.log(Students)
+
+let Info={
+    age:10,
+    name:"Ahmad",
+    email:"ali@gmail.com"
+}
+
+Info.email="check@gmail.com";
+console.log(Info)
