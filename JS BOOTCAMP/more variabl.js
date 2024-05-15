4d15f253d543470c0e0f4f2a6a153b6fd33b28aa
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2024-05-14 19:41:17 -0700

    js file add

---
+++ JS BOOTCAMP/more variabl.js
@@ -0,0 +1,9 @@
+//1.you cant define a variable more than one
+let petname='jhulla'
+petname='spoti'
+console.log(petname)
+//2. there are rules to related variable name
+let a=2
+let result=3+7+a
+console.log(result)
+//3. variable name cannot be resered keyworde
