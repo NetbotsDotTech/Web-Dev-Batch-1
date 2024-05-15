e1b675549f50273b28cb456899a3228e6ea2167d
najmulhassan00 <najmjani6@gmail.com>
2024-05-15 18:47:37 +0500

    new file added

---
+++ js/test.js
@@ -0,0 +1,45 @@
+
+//    Question=agr hum $ ma value string store kry to output ma $ b string ma ata hy
+    
+       
+       //Varible 
+// var studentInfo={
+//     name:"najmul hassan",
+//     age:20,
+//    $:"dolarSign",
+//     email:"najum22@gmail.com",
+//     ema_l:"najum22@gbjkbjkbjkmail.com",
+    
+// }
+
+// studentInfo.name="changeName";
+// studentInfo.$="hello";
+
+// console.log(studentInfo)
+
+//         const
+// consr studentInfo={
+//     age:20,
+//     name:"Najmul Hassan",
+//     email:"najum22@gmail.com",
+// }
+
+
+
+// studentInfo.name="kamal"
+
+// console.log(studentInfo)
+
+
+let studentInfo={
+    age:20,
+    name:"Najmul Hassan",
+    $:1213,
+    email:"najum22@gmail.com",
+}
+
+
+
+studentInfo.name="kamal"
+
+console.log(studentInfo)
