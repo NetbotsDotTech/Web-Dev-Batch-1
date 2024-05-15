74123ad2e5716508daa1ab875bb28a810be519e0
shahid sermiki <shahiddongs@gmail.com>
2024-05-15 06:29:45 -0700

    js class

---
+++ prac1.js
@@ -0,0 +1,33 @@
+
+
+
+// object 
+const trainee={
+Name:"ali",
+age:20,
+address:"skd",
+}
+
+var person={
+
+    Name:"ahmad",
+    RollNo:44,
+    class:5
+
+}
+
+console.log(person);
+
+
+
+
+trainee.age=30;
+console.log(trainee);
+
+//variable
+const x= 40;
+console.log(x)
+
+// after value change
+//x=50;
+console.log(x);
