5f01bd52ee82c05c90b93b0ebf734641fbe77c17
Firdous Hussain <firdoushussain525@gmail.com>
2024-05-15 18:42:12 +0500

    new file addes

---
+++ java/java.js
@@ -0,0 +1,15 @@
+
+var a='@';
+const b="hello";
+console.log(typeof(a))
+console.log(b)
+
+const studentInfo={
+    name:"Firdous Hussain",
+    age:24,
+    email:"firdoushussain525@gmail.com",
+}
+console.log(studentInfo)
+studentInfo.name="Kamal"
+console.log(studentInfo)
+
