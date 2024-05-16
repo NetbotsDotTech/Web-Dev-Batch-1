cce36ca2a322994c9a79ab96d39cecc0f951b164
qaisar ali <baltiqaisar125@gmail.com>
2024-05-16 19:23:37 +0500

    Basic, Arthematic and Logic Operators in JavaScript this operators are commonly used in JavaScript

---
+++ operatorprac.js
@@ -0,0 +1,38 @@
+let x = 3;
+let y = 4;
+
+console.log(x == y);
+console.log(x != y);
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x === y);
+console.log(x !== y);
+console.log(x && y);
+console.log(x || y);
+console.log(!x);
+console.log(!y);
+console.log(x && !y);
+console.log(!x && y);
+console.log(!x || !y);
+console.log(!x && !y);
+console.log(x || !y);
+console.log(!x || y);
+console.log(!x || !y);
+console.log(!x && !y);
+console.log(!x || !y);
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
+console.log(a%b)
+console.log(a+b)(a-b)
+console.log(a-b)(a+b)
+console.log(a*b)(a/b)
+console.log(a/b)(a*b)
+console.log(a%b)(a%b)
+console.log(a**b)(a**b)
+
