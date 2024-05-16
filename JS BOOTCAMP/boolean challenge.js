2452f34133416883b50375711e3af3770db196ed
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2024-05-15 19:39:50 -0700

    add file

---
+++ JS BOOTCAMP/boolean challenge.js
@@ -0,0 +1,5 @@
+let age=4
+let isChild=age<=7
+let isSenior= age>=70
+console.log(isChild)
+console.log(isSenior)
---
+++ JS BOOTCAMP/variableconst.js
@@ -0,0 +1,9 @@
+const person={
+    userName:"nadeem asharf",
+    Age :12,
+    email:"nadeem@gmail.com"
+}
+console.log(typeof(person.Age))
+person.Age="23"
+console.log(person)
+console.log(typeof(person.Age))
