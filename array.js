63a05554350e4d17d1aac002ee6a9648c9b87cfe
Muhammad Nadeem <nadeemmomini77@gmail.com>
2024-05-16 18:53:50 +0500

    arr.practie

---
+++ array.js
@@ -0,0 +1,17 @@
+//Array
+let arr = [2,"xyz",3,"Nadeem",
+{
+    //Inside array we create an object
+    name : "Muhammad Nadeem",
+    regNo : "S22BSCS005",
+    institution : "NetBots",
+}
+]
+
+
+console.log(arr);
+
+let arr2 = [1,2,3,4,5]
+arr2.push(9)       //adding new element in aray using pop
+console.log(arr2);
+
