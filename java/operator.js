49c5583d8f115a443ee05a8d05658d72882a0d98
Firdous Hussain <firdoushussain525@gmail.com>
2024-05-17 18:29:10 +0500

    updated

--- java/operator.js
+++ java/operator.js
@@ -9,13 +9,25 @@ else{
     console.log("false")
 }
 console.log(c)
-let marks1=60
+let marks1=39
 let marks2=83
 let marks3=65
 
-if(marks1>=70){
-    console.log(marks1,marks2,marks3)
+if(marks1>=80){
+    console.log("A+")
+}
+else if(marks1>=70 && marks1<=79){
+    console.log("A")
+}
+else if(marks1>=60 && marks1<=69){
+    console.log("B")
+}
+else if(marks1>=50 && marks>=59){
+    console.log("C")
+}
+else if(marks1<=40 && marks1>=49){
+    console.log("D")
 }
 else{
-    console.log("B")
+    console.log("Fail")
 }
