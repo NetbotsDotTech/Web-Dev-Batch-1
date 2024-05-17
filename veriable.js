bc9a39ca0a442d3cbd3565b3735a7d15c7244174
osamasaifullah <usamasaif4488@gmail.com>
2024-05-17 17:43:20 +0100

    mm

---
+++ veriable.js
@@ -0,0 +1,11 @@
+// function my_function(p,q){
+// return p*q
+// }
+// console.log(my_function(3,4))
+let x=my_function(3,4)
+function my_function(r,s){
+    //console.log(r)
+ return r*s
+   // console.log(p)
+}
+console.log(x)
