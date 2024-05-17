47eae39c0db6b91f4b98ee99aadc3bad4bdd1a2e
qaisar ali <baltiqaisar125@gmail.com>
2024-05-17 17:22:59 +0500

    ifelsecondition

---
+++ Ifelsecondition.js
@@ -0,0 +1,21 @@
+let a= 3;
+let b= 5;
+
+if(a<b){
+    console.log("a is less than b");
+}
+else if (a>b){
+    console.log("a is greater than b");
+}
+else if(a===b){
+    console.log("a is equal to b");
+}
+else if(a!=b){
+    console.log("a is not equal to b")
+}
+else if (b!=a){
+    console.log("b is not equal to a")}
+
+    else{
+        console.log("invalid")
+    }
