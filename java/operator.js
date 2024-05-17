043e5cbab85fbb4c386da976a3efb6a14821627c
Firdous Hussain <firdoushussain525@gmail.com>
2024-05-17 17:44:38 +0500

    update

--- java/operator.js
+++ java/operator.js
@@ -9,3 +9,13 @@ else{
     console.log("false")
 }
 console.log(c)
+let marks1=60
+let marks2=83
+let marks3=65
+
+if(marks1>=70){
+    console.log(marks1,marks2,marks3)
+}
+else{
+    console.log("B")
+}
