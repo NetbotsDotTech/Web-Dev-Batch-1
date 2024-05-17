c8cf1167f5e57af9bbc28392e540a7814fee4366
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2024-05-16 20:50:36 -0700

    function file

---
+++ JSBOOTCAMP/functiondecleration.js
@@ -0,0 +1,10 @@
+//first of writting function
+let  myFunction=function(a,b){
+    result=a*b
+    console.log(result)
+}
+myFunction(8,9)
+//second type of writtig function
+function toCelsius(fahrenhied){
+    
+}
--- node.js
+++ node.js
@@ -1,2 +1,1 @@
-var a =1;
-console.log(a);
+consolse.log(123)
