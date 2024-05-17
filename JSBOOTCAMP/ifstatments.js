4723f8d7bd6b6e09c7a91a7f74be63a4af580dce
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2024-05-16 19:42:03 -0700

    add modify fils

---
+++ JSBOOTCAMP/ifstatments.js
@@ -0,0 +1,12 @@
+let childAge=4
+let youngAge=34
+let seniorAge=90
+if (childAge<=7){
+    console.log("you get discounted")
+}
+if (seniorAge>=70){
+    console.log("you get discounted")
+}
+else{
+    console.log("you get premium rate")
+}
