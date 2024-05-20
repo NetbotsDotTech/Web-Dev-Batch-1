a07a1fe3e159b549d4e3504f26bf326cb6389278
qaisar ali <baltiqaisar125@gmail.com>
2024-05-20 17:12:32 +0500

    array

---
+++ practice.js
@@ -0,0 +1,24 @@
+let arr = [
+    22,"Qaisar","ustad",
+    {
+        name:"Ali",
+        age:22,
+        grade:"A",
+    }
+]
+console.log(arr[3].name="Umar");
+
+let a = 10;
+let b = "10";
+
+let sum = a + b;
+console.log(sum);
+
+if(a===b){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+
+
