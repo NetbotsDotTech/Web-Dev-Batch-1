9ec8eaa51ff027c6967ecdabba90a0df788c1798
osamasaifullah <usamasaif4488@gmail.com>
2024-05-21 16:44:28 +0100

    df

--- html.practice/operator2.js
+++ html.practice/operator2.js
@@ -1,8 +1,11 @@
-var num1=7
-if(num1%2===0){
-    console.log("the number you enter is even")
+// var num1=7
+// if(num1%2===0){
+//     console.log("the number you enter is even")
  
    
-}
-else
-console.log("the number you enter is not odd")
+// }
+// else
+// console.log("the number you enter is not odd")
+let x = -100;
+x >= 5;
+console.log(x)
