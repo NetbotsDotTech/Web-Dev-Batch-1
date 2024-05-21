5053ec1b06319ddadf69c7f786c4f6d922471778
Muhammad Nadeem <nadeemmomini77@gmail.com>
2024-05-21 08:59:56 +0500

    String method in js

---
+++ string-Method.js
@@ -0,0 +1,37 @@
+
+            //string Methods
+
+console.log("*********************************************************************************");
+let name = "Muhammad Nadeem";
+console.log("The length of string is : ",name.length);
+console.log("The string is convert into uppercase : ",name.toUpperCase());
+console.log("The string is convert into uppercase : ",name.toLowerCase());
+
+console.log("*********************************************************************************");
+let myName = "    my name is muhammad nadeem      ";
+console.log(myName.trim());//The trim keyword id used to remove white spaces in strat and last of string 
+
+//slice
+console.log("*********************************************************************************");
+let  institution = "University of Baltistan";
+console.log("Slice is used to cut the string into pieces : ",institution.slice(0,10));
+
+//concat
+console.log("*********************************************************************************");
+let firstName = "Muhammad Nadeem ";
+let lastName = "Momeni";
+console.log("concat is used to concatenate two strings : ",firstName.concat(lastName));
+
+//replace
+console.log("*********************************************************************************");
+let myHobbys = "cricket,football,hockey";
+console.log("The replace keyword is used to replace the string : ",myHobbys.replace("football","voleball"));
+
+console.log("*********************************************************************************");
+
+//charAt
+let myChar = "I love java script";
+console.log("To check which character is at any index : ",myChar.charAt(5));
+
+console.log("*********************************************************************************");
+
