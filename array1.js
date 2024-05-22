77bea048a25f313b40d04ecb7ca13d85ae30d3d7
osamasaifullah <usamasaif4488@gmail.com>
2024-05-22 17:03:33 +0100

    array

---
+++ array1.js
@@ -0,0 +1,15 @@
+arr=new Array();
+console.log(arr)
+arr3=["firstelement","secondelement","thirdelement"];
+
+console.log(arr3)
+arr1=new Array("usama","saif","ali")
+console.log(arr1)
+console.log(arr1[0])
+console.log(arr1[arr1.length-2])
+arr2=new Array(2)
+arr2[0]=1
+arr2[1]=2
+arr2[2]=3
+arr2[3]=5
+console.log(arr2.length)
