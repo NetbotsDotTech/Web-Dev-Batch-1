aa245a48dc07245ba67fce0712d26fa8913947a3
najmulhassan00 <najmjani6@gmail.com>
2024-05-22 13:04:03 +0500

    added a new file slice js

---
+++ js/sclice.js
@@ -0,0 +1,9 @@
+//sclice strings
+
+const n="MUHAMMAD HASSAN";
+let h=n.slice(2,5);
+console.log(h);
+
+const hassan="ali, hassan, 000, 5555, 6666, kashif, hamid";
+let ali=hassan.slice(0,25);
+console.log(ali);
