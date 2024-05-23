90eacbc0de08f4bbfaeca42ac7502c094a27a6a9
Turabi47 <aktura47@gmail.com>
2024-05-23 17:31:20 +0500

    dfk

---
+++ methods.js
@@ -0,0 +1,20 @@
+let text = "asdfghjkl"
+
+let arr1 = [1, 2, 3, 4, 5]
+
+let obj = {
+    intkey: 123,
+    strkey: "asdfghjk",
+    obj: {
+        car: "ferrari",
+        bike: "kawasakiNinja"
+    }
+    }
+
+    // console.log(text.length)
+    // console.log(arr1.length)
+    // console.log(obj.length)
+  
+    console.log(text.at(-2))
+    console.log(arr1.length)
+    console.log(obj.length)
