4692e8eb0681eb1be3e4cdbc96960e39f357b890
Turabi47 <aktura47@gmail.com>
2024-05-23 17:48:40 +0500

    dfk

--- methods.js
+++ methods.js
@@ -15,6 +15,16 @@ let obj = {
     // console.log(arr1.length)
     // console.log(obj.length)
   
-    console.log(text.at(-2))
+    // console.log(text.charAt(2))
+    // console.log(text.at(-2))
+    // console.log("Code of a is:",text.charCodeAt(1))
+    // console.log("Code of s is:",text.charCodeAt(2))
+    // console.log("Code of d is:",text.charCodeAt(3))
+    // console.log(text.slice(2,5));
+    // console.log(text.slice(-5,-1));
+
+    console.log(text.substring(-3, 5))
+
+
     console.log(arr1.length)
     console.log(obj.length)
