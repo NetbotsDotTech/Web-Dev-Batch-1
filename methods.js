d0529927370d3de0867b731e3b27c223f3cd2e68
Turabi47 <aktura47@gmail.com>
2024-05-23 17:52:55 +0500

    dk

--- methods.js
+++ methods.js
@@ -11,19 +11,19 @@ let obj = {
     }
     }
 
-    // console.log(text.length)
-    // console.log(arr1.length)
-    // console.log(obj.length)
+    console.log(text.length)
+    console.log(arr1.length)
+    console.log(obj.length)
   
-    // console.log(text.charAt(2))
-    // console.log(text.at(-2))
-    // console.log("Code of a is:",text.charCodeAt(1))
-    // console.log("Code of s is:",text.charCodeAt(2))
-    // console.log("Code of d is:",text.charCodeAt(3))
-    // console.log(text.slice(2,5));
-    // console.log(text.slice(-5,-1));
+    console.log(text.charAt(2))
+    console.log(text.at(-2))
+    console.log("Code of a is:",text.charCodeAt(1))
+    console.log("Code of s is:",text.charCodeAt(2))
+    console.log("Code of d is:",text.charCodeAt(3))
+    console.log(text.slice(2,5));
+    console.log(text.slice(-5,-1));
+    console.log(text.substring(-3, 5)); //Negative values will be treated as zero
 
-    console.log(text.substring(-3, 5))
 
 
     console.log(arr1.length)
