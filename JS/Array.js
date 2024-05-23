4eae0ed07dc2881f16a6d66cd8c056c3e589e29d
Abdul Ghaffar <abdulkharfaqi30@gmail.com>
2024-05-23 18:29:13 +0500

    add array method

---
+++ JS/Array.js
@@ -0,0 +1,11 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// // console.log(fruits.join(" * "))
+// console.log("after pop")
+// fruits.pop()
+// // console.log(fruits)
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.push("Kiwi"))
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
+console.log(fruits)
