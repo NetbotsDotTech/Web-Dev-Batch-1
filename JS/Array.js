2cafd9261f9f1054e9cdc5da367deed948299a14
Abdul Ghaffar <abdulkharfaqi30@gmail.com>
2024-05-24 10:43:51 +0500

    array update

--- JS/Array.js
+++ JS/Array.js
@@ -3,9 +3,45 @@
 // console.log("after pop")
 // fruits.pop()
 // // console.log(fruits)
+
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // console.log(fruits.push("Kiwi"))
 
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-fruits.shift();
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.shift();
+// console.log(fruits)
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.unshift("Lemon");
+// console.log(fruits)
+
+// changing element in Array.
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits[2] = "Kiwi"
+// console.log(fruits)
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits[fruits.length] = "Kiwi";
+// console.log(fruits)
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// delete fruits[0]
+// console.log(fruits)
+
+// concat()...fruits
+
+// const myGirls = ["Abdul Ghaffar", "Ghaffari"];
+// const myBoys = ["Ishrat", "Zehra", "Mehwish"];
+
+// const myChildren = myGirls.concat(myBoys);
+// console.log(myChildern = myGirls.concat(myBoys))
+
+// const arr1 = ["Cecilie", "Lone"];
+// const arr2 = ["Emil", "Tobias", "Linus"];
+// const arr3 = ["Robin", "Morgan"];
+// console.table( myChildren = arr1.concat(arr2, arr3));
+
+const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+fruits.copyWithin(2, 0);
 console.log(fruits)
