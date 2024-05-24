9dc8719930746e3d8f1221ebc42b3a03157600d6
SairaImran <sairashigri7786@gmail.com>
2024-05-24 16:34:25 +0500

    obj-methd

--- test.js
+++ test.js
@@ -3,10 +3,15 @@ const person={
     lname:"Batool",
     age:20,
     eyecolor:"Blue",
-    family : ["saira","maira","bushra","maryam"]
+    family : ["saira","maira","bushra","maryam"],
+    distance:function(){
+        return 20;
+    }
 };
 
 let text=Object.entries(person);
 console.log(text);
 
 console.log(person.family[1])
+
+console.log(person.distance())
