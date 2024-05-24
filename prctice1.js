7af079586d0a556d05d3a24c6299fbf78b244ac1
SairaImran <sairashigri7786@gmail.com>
2024-05-24 16:01:34 +0500

    object methods

--- prctice1.js
+++ prctice1.js
@@ -1,4 +1,8 @@
-var arr=["saira","fizza","rehana","zuleekha"]
-console.log(arr )
-arr.reverse();
-console.log(arr)
+const person={
+    fname:"Saira",
+    lname:"Batool",
+    age:20,
+    eyecolor:"Blue"
+};
+let text=Object.entries(person);
+console.log(text)
---
+++ test.js
@@ -0,0 +1,12 @@
+const person={
+    fname:"Saira",
+    lname:"Batool",
+    age:20,
+    eyecolor:"Blue",
+    family : ["saira","maira","bushra","maryam"]
+};
+
+let text=Object.entries(person);
+console.log(text);
+
+console.log(person.family[1])
