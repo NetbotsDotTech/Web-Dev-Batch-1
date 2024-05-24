8d3520b33d5ed581a3bdd101a3cd722fc10aa78c
SairaImran <sairashigri7786@gmail.com>
2024-05-24 17:18:39 +0500

    object in array

---
+++ prctc2.js
@@ -0,0 +1,17 @@
+var student = new Object();
+student.Frstname = 'Muhammad';
+student.scondname = 'hussain';
+student.age = 25;
+
+console.log(student)
+
+var teacher = [
+    { Name: "nadia", age: 20, class: 12 },
+    { scndnm: "fauzia", age: 22, class: 12 },
+    { lstnm: "haseena", age: 19, class: 11 }
+]
+for (var a = 0; a < teacher.length; a++) {
+    console.log(teacher[a])
+}
+
+console.log(teacher)
--- prctice1.js
+++ prctice1.js
@@ -8,8 +8,12 @@ const person = {
         scndname: "fizza",
         thrdnme: "rehana",
         lstnme: "zuleekha"
+    },
+    fullname : function(){
+        return this.fname+" "+this.lname;
     }
 };
-let text = Object.entries(person);
-console.log(text)
-console.log(person.objct)
+// let text = Object.entries(person);
+// console.log(text)
+// console.log(person.objct)
+console.log(person.fullname())
