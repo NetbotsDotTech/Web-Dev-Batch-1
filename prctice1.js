3adb3899d7dc4340e487cbf01ce5f97d16962c85
SairaImran <sairashigri7786@gmail.com>
2024-05-24 16:41:58 +0500

    object in object

--- prctice1.js
+++ prctice1.js
@@ -1,8 +1,15 @@
-const person={
-    fname:"Saira",
-    lname:"Batool",
-    age:20,
-    eyecolor:"Blue"
+const person = {
+    fname: "Saira",
+    lname: "Batool",
+    age: 20,
+    eyecolor: "Blue",
+    objct: {
+        frstname: "saira",
+        scndname: "fizza",
+        thrdnme: "rehana",
+        lstnme: "zuleekha"
+    }
 };
-let text=Object.entries(person);
+let text = Object.entries(person);
 console.log(text)
+console.log(person.objct)
