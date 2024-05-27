ca24c5af895cfbb6e616897f9b3da2eccf4b3ae9
osamasaifullah <usamasaif4488@gmail.com>
2024-05-27 17:54:52 +0100

    arrays

---
+++ concat.js
@@ -0,0 +1,4 @@
+arr1=["usama","ali","saif","wali"]
+arr2=["asim","jasim","qamer"]
+arr3=arr1.concat(arr2)
+console.log(arr3)
---
+++ some.js
@@ -0,0 +1,8 @@
+emp={
+    Name:"usama",
+    clu:"10th",
+    selery:1000
+
+};
+console.log(delete emp.clu)
+console.log(emp)
