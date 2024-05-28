820183dbb63fef49c0edf4ff7ddd2e9ab5896f11
Muhammad Nadeem <nadeemmomini77@gmail.com>
2024-05-28 20:02:17 +0500

    object method

---
+++ object_objectMethod.js
@@ -0,0 +1,25 @@
+
+//Object decleration
+
+let person = {
+    name : "Muhammad Nadeem",
+    age : 22,
+    id : 101,
+    phone_no : "03421713449",
+
+};
+console.log(person);
+
+//Nested Object
+
+myObj = {
+    name:"John",
+    age:30,
+    myCars: {
+      car1:"Ford",
+      car2:"BMW",
+      car3:"Fiat"
+    }
+  };
+  console.log(myObj);
+  console.log(myObj.myCars);
