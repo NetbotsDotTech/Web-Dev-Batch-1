05e14214795a85e4b9470b011c31fdd18f22029d
shahid sermiki <shahiddongs@gmail.com>
2024-05-29 05:45:29 -0700

    resoptr

--- restopr.js
+++ restopr.js
@@ -1,20 +1,14 @@
 
-let a=[1,2,3],
-b=[4,5,6,7],
-c=[];
-
-c.concat(a,b)
-
-console.log(c)
-
-
-// JavaScript code for concat() method
-function func() {
-	let num1 = [11, 12, 13],
-		num2 = [14, 15, 16],
-		num3 = [17, 18, 19];
-	console.log(num1.concat(num2, num3));
-    console.log(...num1,...num2,...num3);
-
-}
-func();
+ 
+function myFun(a, b, ...manyMoreArgs) {
+    console.log("a", a);
+    console.log("b", b);
+    console.log("manyMoreArgs", manyMoreArgs);
+  }
+  
+  myFun("one", "two", "three", "four", "five", "six");
+  
+  // a, "one"
+  // b, "two"
+  // manyMoreArgs, ["three", "four", "five", "six"] <-- an array
+  
