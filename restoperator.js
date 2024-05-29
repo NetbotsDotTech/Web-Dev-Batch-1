3d883ee8f18a502410c752609c7fd548dca0b00a
sherbazzahid123@gmail.com <sherbazzahid123@gmail.com>
2024-05-29 16:59:18 +0500

    updated

--- restoperator.js
+++ restoperator.js
@@ -1,13 +1,22 @@
-// const numbers = [1, 2, 3, 4, 5];
+//  const numbers = [1, 2, 3, 4, 5];
 
-// const [first, ...rest] = numbers;
+//  const [first, ...rest] = numbers;
 
-// console.log(first); 
+//  console.log(first); 
 // console.log(rest)
 
-const person = { name: 'sherbaz', age: 20, city: 'skardu' };
 
-const { name, ...rest } = person;
 
-console.log(name); 
-console.log(rest);  
+// const person = { name: 'sherbaz', age: 20, city: 'skardu' };
+
+// const { name, ...rest } = person;
+
+// console.log(name); 
+// console.log(rest);  
+
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { c: 3, d: 4 };
+
+const mergedObject = { ...obj1, ...obj2 };
+console.log(mergedObject); 
