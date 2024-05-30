3bfe76e600e96935cf02a00a20e34beb7e7f044c
SairaImran <sairashigri7786@gmail.com>
2024-05-30 18:37:23 +0500

    new file

---
+++ restop.js
@@ -0,0 +1,16 @@
+// function sum(Name, ...args) {
+//     console.log(arguments)
+//     let sum = 0;
+//     for (let i in args) {
+//         sum += args[i];
+//     }
+//     console.log(sum);
+// }
+// sum("Saira batool", 20, 10, 20);
+
+
+const vehicles = ['BMW', 'N-3506', 'Premio'];// old way
+const car = vehicles[0];
+const truck = vehicles[1];
+const suv = vehicles[2];
+console.log(car,truck,suv)
---
+++ spreadop.js
@@ -0,0 +1,33 @@
+// const myVehicle = {
+//     brand: 'Ford',
+//     model: 'Mustang',
+//     color: 'red'
+// }
+
+// const updateMyVehicle = {
+//     type: 'car',
+//     year: 2021,
+//     color: 'yellow'
+// }
+
+// const myUpdatedVehicle = { ...myVehicle, ...updateMyVehicle }
+// console.log(myUpdatedVehicle);
+
+const numbersOne = [1, 2, 3];
+const numbersTwo = [4, 5, 6];
+const numbersCombined = [...numbersOne, ...numbersTwo];
+console.log(numbersCombined)
+
+
+function sum(Name,...args){
+    console.log(arguments)
+    let sum=0;
+    for(let i in args){
+        sum+=args[i];
+    }
+    console.log(sum);
+}
+let arr=[2,4,5,6,7,5,7,6];
+sum("NetBotTech",...arr)
+sum("Saira batool",20,10,20);
+
