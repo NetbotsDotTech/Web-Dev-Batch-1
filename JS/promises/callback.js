08c0f1ea94c82dccb892776090557679e77db30e
Ehsan Ali <ehsanpkd.19@gmail.com>
2024-06-04 09:53:08 -0700

    updates

--- JS/promises/callback.js
+++ JS/promises/callback.js
@@ -24,24 +24,95 @@
 
 
 
-function Calculator(num1, num2, Callback) {
-  setTimeout(() => {
+// function Calculator(num1, num2, Callback) {
+//   setTimeout(() => {
     
-    let sum = num1 + num2;
-    Callback(sum);
+//     let sum = num1 + num2;
+//     Callback(sum);
+//   }, 3000);
+// }
+
+// function Mul(num1, num2, Callback) {
+//   setTimeout(() => {
+    
+//     let mul = num1 * num2;
+//     Callback(mul);
+//   }, 2000);
+// }
+
+// function Displayer(a)
+// {
+//   console.log(a);
+// }
+// Calculator(5, 5, Displayer);
+
+
+
+// function fetchData(innerFunction) {
+//   console.log("Fetching Data...!");
+//   setTimeout(() => {
+//     console.log("Data fetched");
+//     innerFunction()
+//   }, 4000);
+// }
+
+// function processData(innerFunction) {
+//   console.log("Processing Data...!");
+//   setTimeout(() => {
+//     console.log("Data processed");
+//     innerFunction()
+//   }, 3000);
+// }
+
+// function saveData(innerFunction) {
+//   console.log("Saving Data...!");
+//   setTimeout(() => {
+//     console.log("Data saved");
+//     innerFunction()
+//   }, 2000);
+// }
+
+// fetchData(()=>{
+//   processData(()=>{
+//     saveData(()=>{
+//       console.log("All done!");
+//     })
+//   })
+// })
+
+
+
+function Add(callback) {
+  console.log("Adding Data...!");
+  setTimeout(() => {
+    const sum = 6+5; 
+    callback(sum)
+  }, 4000);
+}
+
+function mul(callback) {
+  console.log("Multiplying Data...!");
+  setTimeout(() => {
+    const mult=5*5;
+    callback(mult)
   }, 3000);
 }
 
-function Calculator(num1, num2, Callback) {
+function sub(callback) {
+  console.log("Subtracting Data...!");
   setTimeout(() => {
-    
-    let mul = num1 * num2;
-    Callback(mul);
+    const sub =11-4;
+    callback(sub)
   }, 2000);
 }
 
-function Displayer(a)
-{
-  console.log(a);
-}
-Calculator(5, 5, Displayer);
+Add((a)=>{
+  console.log(a)
+  mul((m)=>{
+    console.log(m)
+    sub((b)=>{
+      console.log(b)
+      console.log("All done!");
+    })
+  })
+})
