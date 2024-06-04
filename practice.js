1d04e4c6843be93c81243da332387e8dce417d10
SairaImran <sairashigri7786@gmail.com>
2024-06-04 17:08:45 +0500

    asyncawait,promises,callback

--- practice.js
+++ practice.js
@@ -1,9 +1,22 @@
-let Academy={
-    Name :"NetBot.Tech",
-    Course : "Full StacK Web-Development"
+// let Academy={
+//     Name :"NetBot.Tech",
+//     Course : "Full StacK Web-Development"
     
-}
+// }
 
-let {Name:StartName=aDefault,Course:OfferingCourse}=Academy;
+// let {Name:StartName=aDefault,Course:OfferingCourse}=Academy;
 
-console.log(" "+StartName+"\n",OfferingCourse);
+// console.log(" "+StartName+"\n",OfferingCourse);
+
+async function populate(){
+    const requestURL="file:///C:/Users/Administrator/Desktop/bluetooth_content_share%20(2).html";
+    const request=new Request(requestURL);
+
+    const response=await fetch(request);
+    const superbHrs=await response.json();
+
+    populateHeader(superbHrs);
+    populateHrs(superbHrs);
+
+} 
+console.log(populate());
