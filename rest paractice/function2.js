97f8ce396c818cf260ba9cab6ae8817f823f358c
osamasaifullah <usamasaif4488@gmail.com>
2024-06-04 21:07:24 +0100

    practice

---
+++ rest paractice/function2.js
@@ -0,0 +1,8 @@
+function my_function(){
+    console.log("well come to netboot")
+    console.log("well come to netboot")
+    console.log("well come to netboot")
+}
+for(let i=0;i<5;i++){
+    my_function()
+}
