17b04958444024dbeb964ae9d0f5ac472a6c6ce1
ShahidNoori <142967190+ShahidNoori@users.noreply.github.com>
2024-06-10 16:14:38 +0500

    Assigment 1 + Q1

---
+++ Assigments/Assigment#1/Q1.js
@@ -0,0 +1,13 @@
+let a=10;
+var b=10
+const p=3.14
+console.log("Let a= ",a)
+console.log("Var b= ",b)
+console.log("Consr p=",p)
+//Changing Values:
+a=90;
+b=99;
+// p=100; We can't change the const if so it will show Error
+console.log("Let a= ",a) //New Value
+console.log("Var b= ",b) //New Value
+console.log("Consr p=",p) //Const never be changed
---
+++ js_test.js
@@ -0,0 +1,9 @@
+// var global="Shahid";
+// var global="Noori"
+// console.log(global)
+//  let name="Shahid";
+//  name="a"   //can be updated
+//  console.log(name);
+ const reg="S23BSCS026"
+ reg="S23BSCS002"
+ console.log(reg)
