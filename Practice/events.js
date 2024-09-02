389e7a2af852627430fbe4b2411e070a5823e891
ShahidNoori <142967190+ShahidNoori@users.noreply.github.com>
2024-09-02 14:58:11 +0500

    Reached At Call Back

--- Practice/events.js
+++ Practice/events.js
@@ -4,6 +4,8 @@ button.addEventListener("click",()=>{
 }) 
 //serach browser events mdn for more exploration (mouse and keyboard events)
 
-//EVENT BUBBLING**
-
+/*
+EVENT BUBBLING**
+written in html 
+ */
 
