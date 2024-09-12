a697201d3c91b6a376392f978fcf8cec94130623
netbots <netbotstech@gmail.com>
2024-09-12 05:42:24 -0700

    adde login enpoint

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -38,16 +38,26 @@ export const Register = async (req, res, next) => {
   };
 
 
-//     const user = new User(req.body)
 
-
-//     await user.save();
-//     res
-//         .status(201)
-//         .json({
-//             status: true,
-//             message: 'User created successfully',
-//             data: user
-//         });
-
-// }
+  export const Login = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: 'Email Not Found' });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid  password' });
+      }
+      const payload = { userId: user._id, role: user.roles[0] }; 
+      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1d' });
+    
+      res.cookie('access_token', token, { httpOnly: true }); 
+      res.status(200).json({ message: 'Login successful', Detail : user.email, });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Error logging in',  Error: err });
+    }
+  };
+  
